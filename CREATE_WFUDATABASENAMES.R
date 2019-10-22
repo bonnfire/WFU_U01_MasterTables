734efa568b9215dfdbb2f950d078544b3ad7cd71
Bonnie Lin <b2lin@ucsd.edu>
2019-10-22 10:23:54 -0700

    Create two RData for the database structures, one in list and another in df

--- CREATE_WFUDATABASENAMES.R
+++ CREATE_WFUDATABASENAMES.R
@@ -4,9 +4,17 @@ shipments <- list("Kalivas_Italy" = glimpse(WFU_Kalivas_Italy_test_df),
                     "Mitchell" = glimpse(WFU_Mitchell_test_df),
                     "Olivier_Co" = glimpse(WFU_Olivier_co_test_df), 
                     "Olivier_Oxy" = glimpse(WFU_Olivier_ox_test_df))
+shipments <- lapply(shipments, function(x){
+  x$cohort <- stringr::str_match(x$cohort, "#\\d+")
+  x$cohort <- gsub("#", "Cohort0", x$cohort)
+  return(x)
+})
 shipments_df <- rbindlist(shipments, id = "U01", fill = T)
 
 # experiments <- mapply(append, experiments, "master_table") # by apurva's request
 
 
 save(shipments, file = "shipments.RData")
+save(shipments_df, file = "shipmentswithU01col.RData")
+shipments_df <- rbindlist(shipments, id = "U01", fill = T)
+
