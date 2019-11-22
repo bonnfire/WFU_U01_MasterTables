7cdc98ca6b6ca7c5ad767a5e5428bdd6c97b9fda
Bonnie Lin <b2lin@ucsd.edu>
2019-11-22 12:18:34 -0800

    keep empty rfid columns in and remove rows after the total cell, found stray rfid in oxy that isn't found in oxy/cocaine wfu shipment sheets, and locate naive vs nonnaive animals from oxy

--- u01_spleenceca_shipment.R
+++ u01_spleenceca_shipment.R
@@ -36,6 +36,7 @@ names(jhou_spleen_test) <- mgsub::mgsub(names(jhou_spleen_test),
 ###########################
 ###### OLIVIER ############
 ###########################
+setwd("~/Dropbox (Palmer Lab)/Palmer Lab/Bonnie Lin/U01/20190829_WFU_U01_ShippingMaster")
 olivier_spleen_raw <- read_excel(path = "Olivier Spleens Oxy and Coc 91319.xlsx", col_names = F)
 olivier_spleen_cells_raw <- tidyxl::xlsx_cells(path = "Olivier Spleens Oxy and Coc 91319.xlsx/TissueShipments")
 olivier_spleen_formats_raw <- xlsx_formats(path = "Olivier Spleens Oxy and Coc 91319.xlsx")
@@ -65,7 +66,7 @@ for(i in seq(1, ncol(olivier_spleen_raw), 2)){
   olivier_spleen_list[[j]] <- olivier_spleen_raw[, (i:k)]
 }
 
-# add cohort information
+# add cohort information from the legend provided in the excel sheet and remove after assignment
 for(i in 1:9){
   olivier_spleen_list[[i]]$cohort = as.character(olivier_spleen_list[[10]][i,2])
 } # replacement has 40 rows, data has 39 so it has to be outside the lapply function
@@ -73,9 +74,9 @@ olivier_spleen_list[[10]] <- NULL
 
 # remove na rows and split the cohort into two
 olivier_spleen_list_df <- lapply(olivier_spleen_list, function(df){
+  df <- df[-((grep("^total", df$rfid, ignore.case = T)-1):nrow(df)),]
   df <- df %>% 
-    dplyr::filter(!grepl("^(?!\\d)", rfid, perl = T)) %>% 
-    na.omit(rfid) %>%
+    mutate(row_num_location_in_box = dplyr::row_number()) %>% 
     tidyr::separate(col = cohort, into = c("cohort", "experiment"), sep = " (?=[^ ]+$)") %>% 
     mutate(sex = substring(labanimalid, 1, 1))
 return(df)
@@ -85,7 +86,7 @@ return(df)
 # number of counts as the raw files: all numbers match cohorts 1:8 for cocaine, no cohort 6, and only 3 and 4 for oxy
 ##  olivier_spleen_list_df %>% group_by(cohort, experiment) %>% count
 
-# sex count ## bring this to apurva's attention
+# sex count # a little unevent but not alarming
 olivier_spleen_sex <- olivier_spleen_list_df %>% group_by(cohort, experiment, sex) %>% summarize(sexcount = n())
 ggplot(olivier_spleen_list_df, aes(x = sex, fill = sex)) + 
   geom_histogram(stat = "count") + 
@@ -163,3 +164,51 @@ olivier_spleen_list_df %>%
   dplyr::filter(experiment == "Cocaine", !rfid %in% selfadmin_df$rfid) %>% 
   select(rfid) %>% 
   mutate(nchar = nchar(rfid))
+
+# prepare table for Hannah 
+# to create excel workbooks 
+library(tidyverse)
+library(openxlsx) 
+
+# create dataframes to fill up each sheet
+to_genotype_olivier_cocaine <- olivier_spleen_list_df %>% 
+  dplyr::filter(experiment == "Cocaine") %>% 
+  mutate(to_genotype = ifelse(rfid %in% selfadmin_df$rfid, "yes", 
+                              ifelse(is.na(rfid), NA, "no")))
+
+to_genotype_olivier_oxy <- olivier_spleen_list_df %>% 
+  dplyr::filter(experiment == "Oxycodone") %>% 
+  mutate(to_genotype = ifelse(rfid %in% WFU_Olivier_ox_test_df[which(is.na(WFU_Olivier_ox_test_df$comment)), ]$rfid, "yes", 
+                            ifelse(rfid %in% WFU_Olivier_ox_test_df[which(WFU_Olivier_ox_test_df$comment == "Naive"), ]$rfid, "no", NA))) ## XX CHANGE THIS STATEMENT ONCE GIORDANO / OLIVIER GIVE YOU DATA FOR OXY
+
+exp <- olivier_spleen_list_df$experiment %>% unique
+wb <- createWorkbook() 
+addWorksheet(wb, sheetName = exp[1] )
+addWorksheet(wb, sheetName = exp[2] )
+writeData(wb, exp[1], to_genotype_olivier_cocaine)
+writeData(wb, exp[2], to_genotype_olivier_oxy)
+saveWorkbook(wb, file = "olivier_spleen_cocaine_oxy_to_genotype.xlsx", overwrite = TRUE)
+
+# use this function in the future to print the results of same analyses run on diff dataframes into multiple worksheets on excel 
+# createSpreadsheets <- function(species,r1,r2){
+#   ## Create new workbooks
+#   wb <- createWorkbook() 
+#   
+#   ## Create the worksheets
+#   addWorksheet(wb, sheetName = "Results1" )
+#   addWorksheet(wb, sheetName = "Results2" )
+#   
+#   ## Write the data
+#   writeData(wb, "Results1", r1)
+#   writeData(wb, "Results2", r2)
+#   
+#   ## Save workbook to working directory 
+#   saveWorkbook(wb, file = paste(species,".xlsx", sep=""), overwrite = TRUE)
+# }
+# 
+# ## create spreadsheets by calling our function for each species
+# for(s in exp){
+#   createSpreadsheets(s,to_genotype_olivier_cocaine,to_genotype_olivier_oxy)
+# }
+
+
