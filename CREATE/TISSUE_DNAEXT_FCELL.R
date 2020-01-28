6e7c8041224e0ddaa378fe21efb1ea858bce2cf0
Bonnie Lin <b2lin@ucsd.edu>
2020-01-28 10:25:24 -0800

    create plots for 360 320 380 values

--- CREATE/TISSUE_DNAEXT_FCELL.R
+++ CREATE/TISSUE_DNAEXT_FCELL.R
@@ -17,6 +17,10 @@ flow_cell_original_rip <- flow_cell_original$Sheet1 %>%
   dplyr::filter(grepl("Riptide", Library))
 flow_cell_original_rip %>% mutate_at(vars(one_of("Library", "Flow cell lane")), as.factor) %>% summary()
 
+
+######################## 
+## KHAI EXTRACTION TABLE 
+######################## 
 extractions_khai_df <- lapply(extractions_khai_original, function(x){
   x <- x %>% mutate_at(vars(contains('Date')), ~lubridate::ymd(.))
   return(x)
@@ -29,7 +33,72 @@ extractions_khai_df %>%
   mutate_at(vars(one_of("u01", "comments")), as.factor) %>% 
   summary()
 
+extractions_flowcell <- extractions_khai_df %>% 
+  subset(`sampleid_barcode` %in% flow_cell_original_rip$`Sample ID`)
+
 extractions_khai_df %>% mutate_at(vars(one_of("u01", "comments")), as.factor) %>% 
   summary()
+
+extractions_khai_df <- extractions_khai_df %>% dplyr::filter(u01 != "Template")
+# extractions_khai_df %>% dplyr::filter(u01 != "Template") %>% dplyr::filter(u01!=dnaplatecode) # note that olivier c06 shares with mitchell c01
+
+
+extractions_flowcell %>% 
+  ggplot(aes(x = `260_280`)) + 
+  geom_histogram() + 
+  facet_grid(~ u01) +
+  labs(title = paste0("260_280 values by U01")) +
+  theme(axis.text=element_text(size=20),
+        axis.title=element_text(size=25,face="bold"))
+
+extractions_flowcell %>% 
+  ggplot(aes(x = `u01`, y = `260_280`)) + 
+  geom_boxplot() + 
+  labs(title = paste0("260_280 values by U01")) + 
+  theme(axis.text=element_text(size=20),
+        axis.title=element_text(size=25,face="bold"))
+
+extractions_flowcell %>% 
+    ggplot(aes(x = `260_230`)) + 
+    geom_histogram() + 
+    facet_grid(~ u01) +
+    labs(title = paste0("260_230 values by U01")) + 
+  theme(axis.text=element_text(size=20),
+        axis.title=element_text(size=25,face="bold"))
+
+
+extractions_flowcell %>% 
+  ggplot(aes(x = `u01`, y = `260_230`)) + 
+  geom_boxplot() + 
+  labs(title = paste0("260_230 values by U01"))  + 
+  theme(axis.text=element_text(size=20),
+        axis.title=element_text(size=25,face="bold"))
+
+# extractions_flowcell %>% 
+#   ggplot(aes(x = u01, y = `260_230`, color = comments)) + 
+#   geom_boxplot() + 
+#   facet_grid(~ userid) +
+#   labs(title = paste0("260_230 values by U01 and tech"))
+
+########################### 
+# HANNAH EXTRACTION TABLE #
+###########################
+
+## NONE OF THESE ARE OVERLAPPING WITH THE RIPTIDE ONES 
+extractions_hannah_df <- lapply(extractions_hannah_original, function(x){
+  x <- x %>% mutate_at(vars(contains('Date')), ~lubridate::ymd(.))
+  return(x)
+}) %>% rbindlist(fill = T, use.names = T) 
+names(extractions_hannah_df) <- mgsub::mgsub(tolower(names(extractions_hannah_df)), 
+                                           c("[#]", "[[:space:]]|[.]|[[:punct:]]$", "[[:punct:]]"), 
+                                           c("num", "", "_"))
+
+extractions_hannah_df <- extractions_hannah_df %>% 
+  mutate(sampleid_barcode = coalesce(sampleid, sampleid_barcode)) %>% 
+  select(-sampleid)
+
+extractions_hannah_df %>% 
+  subset(sampleid_barcode %in% flow_cell_original_rip$`Sample ID`) %>% 
+  dim()
 
 ## library in flow cell "Riptide-"01 to 03 seem to be line with the "Olivier"01-03 counts
